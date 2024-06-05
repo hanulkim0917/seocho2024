3f7a84f24492fc2322101b48edacc08a062b334f
hanulkim0917 <wnqwer0917@naver.com>
2024-06-05 15:05:55 +0900

    astnc.js_2

--- async.js
+++ async.js
@@ -1,12 +1,19 @@
 // https://jsonplaceholder.typicode.com
 // user의 이름을 구하는 함수를 작성하시오.
 
+// import { reject } from "async";
+// import { resolve } from "path";
+
 const URL = 'https://jsonplaceholder.typicode.com' ; 
 
 const getUserName = async (userId) => {
     // const res = await fetch(URL + '/users/1');
     const res = await fetch(`${URL}/users/${userId}`);
-    console.log('🚀  getUserName  res:', res)
+    // console.log('🚀  getUserName  res:', res)
+    await new Promise((resolve, reject) => {
+        setTimeout(() => resolve('OK'), 2000);
+    });
+
     const data = await res.json(); 
     console.log('🚀  getUserName  data:', data);
     return data?.name;
